4bc11809731068c906e199db36109af1ebc2c7f6
Divyansh Jain <divyanshjain@Divyanshs-MacBook-Air.local>
2025-01-30 03:07:13 +0530

    Header and Ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import BillDashboard from "./components/BillDashboard";
 function App() {
   return (
     <div className="min-h-screen bg-green-100 text-gray-800">
-      <header className="bg-indigo-700 text-white py-4 text-center text-xl font-bold">
+      <header className="bg-indigo-700 text-white py-4 text-center text-2xl font-bold">
         Bill Manager
       </header>
       <main className="p-4">
